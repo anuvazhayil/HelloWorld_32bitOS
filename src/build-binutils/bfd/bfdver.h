72e4e8fd4bef4b54f14035e9c7ed7df1cd1bd909
anuvazhayil <anuv.1994@gmail.com>
2014-09-20 19:30:16 +0530

    Build cross compiler

---
+++ src/build-binutils/bfd/bfdver.h
@@ -0,0 +1,4 @@
+#define BFD_VERSION_DATE 20121113
+#define BFD_VERSION 223010000
+#define BFD_VERSION_STRING  "(GNU Binutils) " "2.23.1"
+#define REPORT_BUGS_TO "<http://www.sourceware.org/bugzilla/>"
