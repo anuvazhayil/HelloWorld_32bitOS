9f2524b5dbb6746fccc9afa9d32ab065e8002b2f
anuvazhayil <anuv.1994@gmail.com>
2014-09-20 20:26:35 +0530

    Build cross compiler

---
+++ src/build-gcc/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.7.2/configure --prefix=/home/cpt/opt/cross --target=i686-elf --enable-languages=c,c++ --disable-nls --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
